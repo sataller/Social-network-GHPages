cb5161908c8d272090c31408420b7b0c5d6f4f74
sataller <astaller@yandex.ru>
2020-05-14 18:59:01 +0300

    Updates

---
+++ precache-manifest.b19744e32f2d251477a58705ba0833f6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb838115f44bef4561e16e7211c2d409",
+    "url": "/Social-network-GHPages/index.html"
+  },
+  {
+    "revision": "44b87c7a8c32c88f134b",
+    "url": "/Social-network-GHPages/static/css/3.87e992de.chunk.css"
+  },
+  {
+    "revision": "81c52174332c447f81c3",
+    "url": "/Social-network-GHPages/static/css/main.8eef920c.chunk.css"
+  },
+  {
+    "revision": "1328646dea7853a1fbd5",
+    "url": "/Social-network-GHPages/static/js/2.e65d0197.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-network-GHPages/static/js/2.e65d0197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b87c7a8c32c88f134b",
+    "url": "/Social-network-GHPages/static/js/3.c31beef8.chunk.js"
+  },
+  {
+    "revision": "81c52174332c447f81c3",
+    "url": "/Social-network-GHPages/static/js/main.1ee99291.chunk.js"
+  },
+  {
+    "revision": "baf6006b29d9a0d947ee",
+    "url": "/Social-network-GHPages/static/js/runtime-main.f2f141e7.js"
+  },
+  {
+    "revision": "f30dabde1d5de73796e644140af1b5ce",
+    "url": "/Social-network-GHPages/static/media/background1.f30dabde.jpeg"
+  },
+  {
+    "revision": "d842d876c2fd2e6d76a153b78885520d",
+    "url": "/Social-network-GHPages/static/media/preloader1.d842d876.svg"
+  },
+  {
+    "revision": "ae92409ee468c5a88d6366a4898f2de2",
+    "url": "/Social-network-GHPages/static/media/spamIcon.ae92409e.png"
+  }
+]);
